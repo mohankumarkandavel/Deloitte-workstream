ac4f60c3011d55b6d4108c1b3ed14a1f211a2ea4
Kelvin Lau <kclau95@gmail.com>
2017-09-01 10:38:05 +1200

    Remove @ModelAttribute because not implementing the UI

--- src/main/java/nz/co/vincens/service/LoginController.java
+++ src/main/java/nz/co/vincens/service/LoginController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ResponseEntity login(@ModelAttribute("login") Login login) {
+	public ResponseEntity login(Login login) {
 		if (login != null && login.getPassword() != null && login.getUsername() != null) {
 			if (login.getUsername().equals("James") && login.getPassword().equals("123")) {
 				return ResponseEntity.ok().build();
