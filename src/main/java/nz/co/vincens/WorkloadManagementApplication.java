0c1af388b1f63b425e316de8b7dbdcbf1a427d2e
Kelvin Lau <kclau95@gmail.com>
2017-08-31 21:50:21 +1200

    Add main method to start the server

---
+++ src/main/java/nz/co/vincens/WorkloadManagementApplication.java
@@ -0,0 +1,11 @@
+package nz.co.vincens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkloadManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkloadManagementApplication.class, args);
+    }
+}
