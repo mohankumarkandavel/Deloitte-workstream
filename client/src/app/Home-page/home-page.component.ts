26e532027a0617f3a627c957c48b69c50308bb35
MOHAN\mohankumar <mkan839@aucklanduni.ac.nz>
2017-09-15 00:10:50 +1200

    styling taskpage

--- client/src/app/Home-page/home-page.component.ts
+++ client/src/app/Home-page/home-page.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {NgbModal,ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
@@ -15,7 +16,7 @@ export class HomePageComponent implements OnInit {
 
   //open new task popup
   onNewTask(id: string) {
-    this.modalService.open(id);
+    this.modalService.open(id, { windowClass: 'task-modal' });
     console.log("found");
   }
   onAddTask(){
@@ -26,7 +27,7 @@ export class HomePageComponent implements OnInit {
     this.pendingTaskList.push(e.dragData);
     //todo change task status
     this.onRemoveTask(e.dragData, this.draftTaskList);
-    this.modalService.open(id);
+    this.modalService.open(id, { windowClass: 'recommend-modal' });
   }
   onRemoveTask(task: any, list: Array<any>) {
     let index = list.map(function (e) {
@@ -54,13 +55,13 @@ export class HomePageComponent implements OnInit {
         deadLine: "15/09/2017",
         description: "create a web page using angular, hmtl, css,etc",
         status: "",
-        member: "mohan"
+        member: ""
       },
       {
         taskName: "create login page",
         deadLine: "17/09/2017",
         description: "create a web page using angular, hmtl, css,etc",
-        status: "pending",
+        status: "",
         member: ""
       }];
   }
