2ce9d4761b935a76dcc43624e13c909c30cb73a0
Shenghong Huang <discoverytrips@yahoo.com>
2017-09-16 12:19:16 +1200

    Update function based on provided structure

--- server/src/main/java/nz/co/vincens/service/LoginConsole.java
+++ server/src/main/java/nz/co/vincens/service/LoginConsole.java
@@ -6,32 +6,54 @@ import java.util.Scanner;
 
 /**
  * Created on 06/09/2017 by Shenghong Huang .
- * Last modified on 13/09/2017 by Shenghong Huang.
+ * Last modified on 14/09/2017 by Shenghong Huang.
  */
 public class LoginConsole {
-    private static String Input_UserName = null;
+    private static String Input_Username = null;
     private static String Input_Password = null;
+    private static int Output_Id = 0;
     private static String Output_Name = null;
-    private static String Output_RoleName = null;
-    private static String Output_RoleID = null;
+    private static String Output_Rolename = null;
+    private static String Output_Roleid = null;
     private static int Output_Count = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter UserID");
-        Input_UserName = sc.nextLine();
+        System.out.println("Please enter Username");
+        Input_Username = sc.nextLine();
         System.out.println("Please enter Password");
         Input_Password = sc.nextLine();
-        String sql = "CALL UserLogin(" + Input_UserName + "," + Input_Password + ")";
+        String sql = "CALL UserLogin(" + Input_Username + "," + Input_Password + ")";
         ResultSet rs = DatabaseHelper.DatabaseExecution(sql);
         System.out.println("QueryResult：");
         try {
             while (rs.next()) {
-                Output_Name = rs.getString("name");
-                Output_RoleName = rs.getString("rolename");
-                Output_RoleID = rs.getString("roleid");
-                System.out.println("Hi " + Output_Name);
-                System.out.println("You are: " + Output_RoleName);
+                Output_Count = Integer.parseInt(rs.getString("count(*)"));
+                System.out.println("Count: " + Output_Count);
+                // Login faild
+                // No result
+                if (Output_Count == 0) {
+                    System.out.println("Incorrect Username or Password");
+                    return;
+                }
+                // More than one
+                else if (Output_Count > 1) {
+                    System.out.println("System error");
+                    return;
+                }
+                // Correct
+                else if (Output_Count == 1) {
+                    Output_Id = Integer.parseInt(rs.getString("id"));
+                    Output_Name = rs.getString("name");
+                    Output_Roleid = rs.getString("roleid");
+                    Output_Rolename = rs.getString("rolename");
+                    System.out.println("Login successful");
+                    System.out.println("Id: " + Output_Id);
+                    System.out.println("Username: " + Input_Username);
+                    System.out.println("Name: " + Output_Name);
+                    System.out.println("Roleid: " + Output_Roleid);
+                    System.out.println("Rolename: " + Output_Rolename);
+                }
             }
             rs.close();
         } catch (SQLException e) {
