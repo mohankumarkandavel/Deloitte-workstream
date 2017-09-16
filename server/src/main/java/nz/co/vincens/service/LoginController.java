eac0899556f0f5b5f118594b61a2ba5b52ac0db0
Shenghong Huang <discoverytrips@yahoo.com>
2017-09-16 12:21:36 +1200

    Implement login

--- server/src/main/java/nz/co/vincens/service/LoginController.java
+++ server/src/main/java/nz/co/vincens/service/LoginController.java
@@ -8,22 +8,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * API endpoint(s) for logging in.
  */
 @RestController
 public class LoginController {
-
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ResponseEntity login(Login login) {
-		if (login != null && login.getPassword() != null && login.getUsername() != null) {
-			if (login.getUsername().equals("James") && login.getPassword().equals("123")) {
-				return ResponseEntity.ok().build();
-			} else {
-				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-			}
-		} else {
-			return ResponseEntity.badRequest().build();
-		}
-	}
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ResponseEntity login(Login login) {
+        String Username = login.getUsername();
+        String Password = login.getPassword();
+        int Count = 0;
+        if (Username != null && Password != null) {
+            String sql = "CALL UserLogin(" + login.getUsername() + "," + login.getPassword() + ")";
+            ResultSet rs = DatabaseHelper.DatabaseExecution(sql);
+            try {
+                while (rs.next()) {
+                    Count = Integer.parseInt(rs.getString("count(*)"));
+                    if (Count == 1) {
+                        return ResponseEntity.ok().build();
+                    } else {
+                        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+                    }
+                }
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
