9425ace3076a34d05c3a87643b2648677268264d
Shenghong Huang <discoverytrips@yahoo.com>
2017-09-17 19:00:36 +1200

    Fix bugs and add essential comments

--- server/src/main/java/nz/co/vincens/service/DatabaseHelper.java
+++ server/src/main/java/nz/co/vincens/service/DatabaseHelper.java
@@ -3,30 +3,62 @@ package nz.co.vincens.service;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- * Created on 06/09/2017 by Shenghong Huang .
- * Last modified on 13/09/2017 by Shenghong Huang.
+ * This class is used to implement specific functions for database access. Contains the variables required for the
+ * database connection, and the functions required to perform the contents of the database query.
+ * The current database uses the Alibaba Cloud MySQL database service.
+ * Database queries use call procedure functions.
+ * <p>
+ * Created on 06/09/2017 by Shenghong Huang.
+ * Last modified on 16/09/2017 by Shenghong Huang.
  */
 public class DatabaseHelper {
+    // JDBC driver name and database URL
     private static final String DRIVE = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://rm-wz9646cu4gae4478fo.mysql.rds.aliyuncs.com:3306/workload";
+    // Database credentials
     private static final String USER = "root";
     private static final String PASSWORD = "Student2017SoftwareEngineering";
 
+    /**
+     * The function of database execution.
+     * Database queries use call procedure functions.
+     *
+     * @param sql The database execution sentence
+     * @return The result set of database execution
+     * @throws SQLException Handle errors for JDBC
+     * @throws Exception    Handle errors for Class.forName
+     */
     public static ResultSet DatabaseExecution(String sql) {
+        // Create the connection statement
         Connection con = null;
+        // Create the statement
+        Statement statement = null;
         try {
+            // Register JDBC driver
             Class.forName(DRIVE);
+            // Open a connection
             con = DriverManager.getConnection(URL, USER, PASSWORD);
+            // Check connection status
             if (!con.isClosed()) {
                 System.out.println("Succeeded connecting to the Database!");
             }
-            Statement statement = con.createStatement();
-            return statement.executeQuery(sql);
+            // State a MySQL connection object
+            statement = con.createStatement();
+            // Execute a query and return query result
+            ResultSet rs = statement.executeQuery(sql);
+            // Return query result set
+            return rs;
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+            return null;
         } catch (Exception e) {
-            System.out.println("MYSQL Error:" + e.getMessage());
+            // Handle errors for Class.forName
+            e.printStackTrace();
             return null;
         }
     }
--- server/src/main/java/nz/co/vincens/service/LoginConsole.java
+++ server/src/main/java/nz/co/vincens/service/LoginConsole.java
@@ -5,48 +5,65 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 /**
+ * This class is to test database connection.
+ * Some variables are not been used in LoginController.java which is backup for the further usage.
+ * <p>
  * Created on 06/09/2017 by Shenghong Huang .
  * Last modified on 14/09/2017 by Shenghong Huang.
  */
 public class LoginConsole {
+    // The input variables for login
     private static String Input_Username = null;
     private static String Input_Password = null;
+    // The return variables after success login
     private static int Output_Id = 0;
     private static String Output_Name = null;
     private static String Output_Rolename = null;
     private static String Output_Roleid = null;
     private static int Output_Count = 0;
 
+    /**
+     * The function of database execution.
+     * Database queries use call procedure functions.
+     *
+     * @throws SQLException Handle errors for JDBC
+     */
     public static void main(String[] args) {
+        // Input login user login information
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter Username");
         Input_Username = sc.nextLine();
         System.out.println("Please enter Password");
         Input_Password = sc.nextLine();
+        // Generate database query sentence
         String sql = "CALL UserLogin(" + Input_Username + "," + Input_Password + ")";
+        // Call the function of database query operation
         ResultSet rs = DatabaseHelper.DatabaseExecution(sql);
         System.out.println("QueryResult：");
         try {
+            // Extract data from result set
             while (rs.next()) {
+                // Login query should only have one row of result
                 Output_Count = Integer.parseInt(rs.getString("count(*)"));
                 System.out.println("Count: " + Output_Count);
-                // Login faild
-                // No result
+                // If it return zero row of result
                 if (Output_Count == 0) {
                     System.out.println("Incorrect Username or Password");
                     return;
                 }
-                // More than one
+                // If it return more than one result
                 else if (Output_Count > 1) {
                     System.out.println("System error");
                     return;
                 }
-                // Correct
+                // If it return only one row of result
                 else if (Output_Count == 1) {
+                    // Retrieve by column name
                     Output_Id = Integer.parseInt(rs.getString("id"));
                     Output_Name = rs.getString("name");
                     Output_Roleid = rs.getString("roleid");
                     Output_Rolename = rs.getString("rolename");
+                    // Display values
                     System.out.println("Login successful");
                     System.out.println("Id: " + Output_Id);
                     System.out.println("Username: " + Input_Username);
@@ -55,8 +72,10 @@ public class LoginConsole {
                     System.out.println("Rolename: " + Output_Rolename);
                 }
             }
+            // Clean-up environment
             rs.close();
         } catch (SQLException e) {
+            //Handle errors for JDBC
             e.printStackTrace();
         }
     }
