f87ba72a32791701ae022d0ccf0ff8798acee0b8
Fraser <lewissmith.fraser@gmail.com>
2017-09-20 06:25:30 +1200

    Merge branch 'improve_loginPage' into feature/integration Add client.iml to gitignore
    client.iml is a IDEA specific folder so should not be pushed to the repository
    Fix bug in angular-cli.json
    
    Removed a trailing comma on line 27
    Update package-lock.json
    
    Did not have dependency for jquery added

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
