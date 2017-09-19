3f75b2fcdcfd0eab7b3c9f3ffe03340f3331cd54
Kelvin Lau <kclau95@gmail.com>
2017-09-20 10:46:45 +1200

    Add simple population of tasks

--- client/src/app/tasks/tasks.component.ts
+++ client/src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-tasks',
@@ -9,11 +10,21 @@ import { Router } from '@angular/router';
 })
 export class TasksComponent implements OnInit {
 
-  constructor(private modalService: NgbModal, private router: Router) {
+  constructor(private modalService: NgbModal, private router: Router, private http: Http) {
   }
 
+  private tasks: any[];
+
   ngOnInit() {
+    this.http.get("http://localhost:8080/task" ).subscribe(
+      (response) => {
+        if(response.ok) {
+          this.tasks = JSON.parse(response.text());
+        }
+      }
+    );
   }
+
   newTask(id: string){
     this.modalService.open(id);
     console.log("found");
