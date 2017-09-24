b430bc7408aac8e33789f9d49757df3a24946edc
Arhdel <brentbwu@gmail.com>
2017-09-24 13:13:07 +1300

    error handling for password

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -34,7 +34,7 @@ export class LoginComponent implements OnInit {
           this.router.navigateByUrl("/tasks");
         }
       },
-      (error) => alert("Password is wrong!"),
+      (error) => console.log(`Error:${error.toString()}`),
       () => console.log("Complete")
     );
   }
