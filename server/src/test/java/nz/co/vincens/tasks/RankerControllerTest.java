3947a3e3b77556cedc54348dc271669ad6bc3650
Kelvin Lau <kclau95@gmail.com>
2017-09-28 11:36:26 +1300

    Fix RankerControllerTest
    Injecting TaskService works now. Task id will need to be fixed later for correct id matching. Will also need to inject UserService later

--- server/src/test/java/nz/co/vincens/tasks/RankerControllerTest.java
+++ server/src/test/java/nz/co/vincens/tasks/RankerControllerTest.java
@@ -2,14 +2,17 @@ package nz.co.vincens.tasks;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import nz.co.vincens.login.UserService;
 import nz.co.vincens.model.Attribute;
 import nz.co.vincens.model.Group;
 import nz.co.vincens.model.Status;
 import nz.co.vincens.model.Task;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,28 +23,35 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Date;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
 
-// @RunWith(SpringRunner.class)
-// @WebMvcTest(RankerController.class)
+@RunWith(SpringRunner.class)
+@WebMvcTest(RankerController.class)
 public class RankerControllerTest {
 
-    // @Autowired
-    // private MockMvc mockMvc;
+    @MockBean private TaskService taskService;
+    // TODO: 28/09/2017 will need to set this mock up when taskService uses it (on other branch)
+    // @MockBean private UserService userService;
+    @Autowired private MockMvc mockMvc;
 
     @Test
     public void allAvailable() throws Exception {
-        // FIXME: 21/09/2017 Can't inject TaskService for RankerController
-        // Task task = new Task(0, "Test task", "testing", new Attribute(1, 1, 1, 1), new Date(), Group
-        //         .BUSINESS_AND_DEVELOPMENT, Status.DRAFT, 1);
-        //
-        // Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm a z").create();
-        //
-        // RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rank")
-        //         .content(gson.toJson(task)).contentType(MediaType.APPLICATION_JSON);
-        //
-        // MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-        // assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+        // TODO: 28/09/2017 for later fix ids (on other branch)
+        Task task = new Task(0, "Test task", "testing", new Attribute(1, 1, 1, 1), new Date(), Group
+                .BUSINESS_AND_DEVELOPMENT, Status.DRAFT, 1, null);
+
+
+        given(taskService.getTask(0)).willReturn(task);
+
+        // TODO: 28/09/2017 need correct date-time format (for later)
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm a z").create();
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/rank/" + 1)
+                .content(gson.toJson(task)).contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
     }
 
     @Test
