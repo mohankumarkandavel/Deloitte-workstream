a2951164e661aed1f732890ca589a9b8069d8148
Kelvin Lau <kclau95@gmail.com>
2017-10-03 20:56:28 +1300

    Fix RankerControllerTest

--- server/src/test/java/nz/co/vincens/tasks/RankerControllerTest.java
+++ server/src/test/java/nz/co/vincens/tasks/RankerControllerTest.java
@@ -4,16 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.internal.LinkedTreeMap;
 import nz.co.vincens.login.UserService;
 import nz.co.vincens.model.Attribute;
 import nz.co.vincens.model.Group;
 import nz.co.vincens.model.Status;
 import nz.co.vincens.model.Task;
-import nz.co.vincens.model.TeamMember;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +24,6 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Date;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -39,18 +34,17 @@ import static org.mockito.BDDMockito.given;
 public class RankerControllerTest {
 
     @MockBean private TaskService taskService;
-    // TODO: 28/09/2017 will need to set this mock up when taskService uses it (on other branch)
-    // @MockBean private UserService userService;
+    // TODO: 28/09/2017 assert more things (based off of what userService will return)
+    @MockBean private UserService userService;
     @Autowired private MockMvc mockMvc;
 
     @Test
     public void allAvailable() throws Exception {
-        // TODO: 28/09/2017 for later fix ids (on other branch)
-        Task task = new Task(0, "Test task", "testing", new Attribute(1, 1, 1, 1), new Date(), Group
+        Task task = new Task(1, "Test task", "testing", new Attribute(1, 1, 1, 1), new Date(), Group
                 .BUSINESS_AND_DEVELOPMENT, Status.DRAFT, 1, null);
 
 
-        given(taskService.getTask(0)).willReturn(task);
+        given(taskService.getTask(1)).willReturn(task);
 
         // TODO: 28/09/2017 need correct date-time format (for later)
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm a z").create();
@@ -60,17 +54,14 @@ public class RankerControllerTest {
 
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
         assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
-
-        // TODO: 28/09/2017 assert more things (based off of what userService will return)
     }
 
     @Test
     public void requireFiveAvailability() throws Exception {
-        // TODO: 28/09/2017 for later fix ids (on other branch)
-        Task task = new Task(0, "Test task", "testing", new Attribute(1, 1, 5, 1), new Date(), Group
+        Task task = new Task(1, "Test task", "testing", new Attribute(1, 1, 5, 1), new Date(), Group
                 .BUSINESS_AND_DEVELOPMENT, Status.DRAFT, 1, null);
 
-        given(taskService.getTask(0)).willReturn(task);
+        given(taskService.getTask(1)).willReturn(task);
 
         // TODO: 28/09/2017 need correct date-time format (for later)
         Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm a z").create();
@@ -80,8 +71,6 @@ public class RankerControllerTest {
 
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
         assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
-
-        // TODO: 28/09/2017 assert more things (based off of what userService will return)
 
         JsonParser jsonParser = new JsonParser();
         JsonElement jsonElement = jsonParser.parse(result.getResponse().getContentAsString());
